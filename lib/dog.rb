81ef2fe8ddac23ea4b356c8764c1581405730cda
Akram_Alam <arundelfootball23@gmail.com>
2020-11-16 23:25:37 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,17 @@
 class Dog 
 
-attr_accessor :name, :breed
+# attr_accessor :name, :breed
+    def breed=(breed)
+        @breed = breed
+    end 
+    def breed
+        @breed 
+    end 
+   def name=(name)
+   @name = name 
+   end 
 
-#    def name=(name)
-#    @name = name 
-#    end 
-
-#    def name 
-#     @name 
-#    end 
+   def name 
+    @name 
+   end 
 end 
